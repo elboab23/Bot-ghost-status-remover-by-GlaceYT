947b61b0e70b175867564e63344b6ce4a408ec3e
elboab23 <elboab23@gmail.com>
2024-12-25 09:07:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["📒 Watching Applications!", "🎢 Walking around the park!"];
+const statusMessages = ["🎄 Watching Home Alone!", "🎁 Merry Christmas!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
